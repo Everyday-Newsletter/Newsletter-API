12c1cb6f0c4d016a375e05b87ed4607a7654ca6a
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-09-16 17:32:05 -0400

    update comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from routes.bored import bored_api
 
 app = Flask(__name__)
 app.register_blueprint(joke_api)  # /joke
-app.register_blueprint(bored_api)  # /bored
+app.register_blueprint(bored_api)  # /activity
 
 # Port 8080 on all interfaces
 app.run("0.0.0.0", port=8080)
