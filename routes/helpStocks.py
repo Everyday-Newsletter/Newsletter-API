1c99d6b55227af4ad207c02a08d7b91aca80257f
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-09-16 19:36:54 -0400

    Update requirements.txt

--- routes/helpStocks.py
+++ routes/helpStocks.py
@@ -8,18 +8,17 @@ import io
 
 
 def get_all_stock_data(ticker):
+    if not _is_valid_symbol(ticker):
+        return None
 
-    if(not _is_valid_symbol(ticker)):
-       return None
-    
     graphs = [get_year(ticker), get_month(ticker), get_intraday(ticker)]
     return graphs
 
 
 def get_year(ticker):
     today = dt.datetime.today()
-    
-    if (today.month > 11):
+
+    if today.month > 11:
         startMonth = today.month - 11
         startYear = today.year
     else:
@@ -27,65 +26,60 @@ def get_year(ticker):
         startYear = today.year - 1
     data = _get_data(dt.datetime(startYear, startMonth, 1), today, ticker)
     my_stringIObytes = io.BytesIO()
-    plt.savefig(my_stringIObytes, format='jpg', bbox_inches='tight')
+    plt.savefig(my_stringIObytes, format="jpg", bbox_inches="tight")
     my_stringIObytes.seek(0)
-    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode('ascii')
+    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode("ascii")
     return my_base64_jpgData
 
 
 def get_month(ticker):
-    
     today = dt.datetime.today()
     now = dt.datetime.now()
-    
-    if (today.month == 1):
+
+    if today.month == 1:
         startMonth = today.month + 11
-        startYear = today.year-1
+        startYear = today.year - 1
     else:
         startMonth = today.month - 1
         startYear = today.year
     data = _get_data(dt.datetime(startYear, startMonth, 1), today, ticker)
     my_stringIObytes = io.BytesIO()
-    plt.savefig(my_stringIObytes, format='jpg', bbox_inches='tight')
+    plt.savefig(my_stringIObytes, format="jpg", bbox_inches="tight")
     my_stringIObytes.seek(0)
-    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode('ascii')
+    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode("ascii")
     return my_base64_jpgData
 
 
 def get_intraday(ticker):
-
     data = yf.download(ticker, period="1d", interval="1m")
     plt.figure(figsize=(10, 5))
-    plt.plot(data['Close'])
-    company_name = yf.Ticker(ticker).info['longName']
-    plt.title(company_name + ': Historical Stock Value')
-    plt.xlabel('Date')
-    plt.ylabel('Stock Price')
+    plt.plot(data["Close"])
+    company_name = yf.Ticker(ticker).info["longName"]
+    plt.title(company_name + ": Historical Stock Value")
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
     my_stringIObytes = io.BytesIO()
-    plt.savefig(my_stringIObytes, format='jpg', bbox_inches='tight')
+    plt.savefig(my_stringIObytes, format="jpg", bbox_inches="tight")
     my_stringIObytes.seek(0)
-    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode('ascii')
+    my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode("ascii")
     return my_base64_jpgData
 
 
-def _get_data(start, end, ticker) :
+def _get_data(start, end, ticker):
     data = yf.download(ticker, start, end)
 
-    #Visualizing the fetched data
+    # Visualizing the fetched data
     plt.figure(figsize=(10, 5))
-    plt.plot(data['Close'])
-    company_name = yf.Ticker(ticker).info['longName']
-    plt.title(company_name + ': Historical Stock Value')
-    plt.xlabel('Date')
-    plt.ylabel('Stock Price')
+    plt.plot(data["Close"])
+    company_name = yf.Ticker(ticker).info["longName"]
+    plt.title(company_name + ": Historical Stock Value")
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
 
     return data
 
 
 def _is_valid_symbol(ticker):
-
-    info = yf.Ticker(ticker).history(
-        period='7d',
-        interval='1d')
+    info = yf.Ticker(ticker).history(period="7d", interval="1d")
 
     return len(info) > 0
