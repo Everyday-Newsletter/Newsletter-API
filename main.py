88ce9ff9526afb1f02460338f83b77403f7d138f
OmOgale <123871192+OmOgale@users.noreply.github.com>
2023-09-16 22:44:48 -0400

    Adding Meme API functionality for single and multiple memes

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ from routes.joke import joke_api
 from routes.bored import bored_api
 from routes.news import news_api
 from routes.stock import stock_api
+from routes.meme import meme_api
+from routes.memes import memes_api
 
 app = Flask(__name__)
 app.register_blueprint(joke_api)  # /joke
 app.register_blueprint(bored_api)  # /activity
 app.register_blueprint(news_api)  # /news
 app.register_blueprint(stock_api) #/stocks
+app.register_blueprint(meme_api) #/meme
+app.register_blueprint(memes_api) #/memes
 
 # Port 8080 on all interfaces
 app.run("0.0.0.0", port=8080)
---
+++ routes/memes.py
@@ -0,0 +1,33 @@
+import random
+import requests
+from flask import request, Blueprint
+
+memes_api = Blueprint("memes_api", __name__)
+
+def get_json(url = None):
+    if not url:
+        return
+    response = requests.get(url)
+    if (response.status_code >= 300 or response.status_code < 200):
+        return
+    return response.json()
+
+def call_url(my_url):
+    caller_url = my_url
+    return get_json(caller_url)
+
+def get_several_memes():
+    url_lst = [call_url(f"https://meme-api.com/gimme/wholesomememes/50"),
+               call_url(f"https://meme-api.com/gimme/memes/50"),
+               call_url(f"https://meme-api.com/gimme/meme/50")
+               ]
+    img_lst = []
+    meme_dict = random.choice(url_lst)
+    meme_lst = meme_dict['memes']
+    valid_lst = list(filter(lambda x: x and not x['nsfw'], meme_lst))
+    img_lst = [a['url'] for a in valid_lst]
+    return img_lst
+
+@memes_api.route("/memes", methods=["GET"])
+def memes():
+    return get_several_memes()
